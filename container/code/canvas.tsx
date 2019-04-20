4a09296f5a0b08f452cfdd6293e82e43b495209e
elagha <elagha@users.noreply.github.com>
2019-04-20 08:27:35 +0200

    Add files via upload

---
+++ container/code/canvas.tsx
@@ -0,0 +1,6 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Donation_item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_FXLCkwfbE", {}, 193,142);
+export const Feed_article = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_CVjnIPYy4", {}, 327,184);
+export const Project_item = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_CkeClpWHj", {}, 285,423);
